ff0d5324e1a6b4d6a74bc989614db429604207a5
Chris Leech <cleech@redhat.com>
2015-10-01 22:48:17 -0700

    QEMU QCoW version 1 handler
    Adapted from the QEMU code.  Handles qcow version 1 with compression
    (read only, copy-on-write expanded) and read-only backing images in
    either qcow or raw format.  Encryption is intentionally not supported,
    as it's a flawed design that has been deprecated in QEMU.
    
    My plans are to extend this to qcow2 next, but qcow was a good small
    module to test porting from QEMU to tcmu-runner.
    
    Signed-off-by: Chris Leech <cleech@redhat.com>

---
+++ qcow.h
@@ -0,0 +1,31 @@
+#ifndef _QCOW_H_
+#define _QCOW_H_
+
+#include <stdint.h>
+
+#define QCOW_MAGIC (('Q' << 24) | ('F' << 16) | ('I' << 8) | 0xfb)
+#define QCOW_VERSION 1
+
+#define QCOW_CRYPT_NONE 0
+#define QCOW_CRYPT_AES  1
+
+#define QCOW_OFLAG_COMPRESSED (1LL << 63)
+
+struct qcow_header
+{
+    uint32_t magic;
+    uint32_t version;
+    uint64_t backing_file_offset;
+    uint32_t backing_file_size;
+    uint32_t mtime;
+    uint64_t size; /* in bytes */
+    uint8_t cluster_bits;
+    uint8_t l2_bits;
+    uint16_t padding;
+    uint32_t crypt_method;
+    uint64_t l1_table_offset;
+} __attribute__((__packed__));
+
+#define L2_CACHE_SIZE 16
+
+#endif /* _QCOW_H_ */
