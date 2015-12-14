9fe82c8be40d2a57a308705c762449cf9aa7e73e
Max Khon <fjoe@samodelkin.net>
2015-12-14 00:00:35 +0000

    - Sample async file handler. Build with -DASYNC_FILE_HANDLER to build async file handler. - CMake glue for sample async file handler.

--- file_example.c
+++ file_example.c
@@ -42,11 +42,108 @@
 
 #include "tcmu-runner.h"
 
+#ifdef ASYNC_FILE_HANDLER
+#include <pthread.h>
+#include <signal.h>
+#include "libtcmu.h"
+
+#define NHANDLERS 2
+#define NCOMMANDS 16
+
+struct file_handler {
+	struct tcmu_device *dev;
+	int num;
+
+	pthread_mutex_t mtx;
+	pthread_cond_t cond;
+
+	pthread_t thr;
+	int cmd_head;
+	int cmd_tail;
+	struct tcmulib_cmd *commands[NCOMMANDS];
+};
+#endif /* ASYNC_FILE_HANDLER */
+
 struct file_state {
 	int fd;
 	uint64_t num_lbas;
 	uint32_t block_size;
+
+#ifdef ASYNC_FILE_HANDLER
+	pthread_mutex_t completion_mtx;
+	int curr_handler;
+	struct file_handler h[NHANDLERS];
+#endif /* ASYNC_FILE_HANDLER */
 };
+
+#ifdef ASYNC_FILE_HANDLER
+static int file_handle_cmd(
+	struct tcmu_device *dev,
+	struct tcmulib_cmd *tcmulib_cmd);
+
+static void *
+file_handler_run(void *arg)
+{
+	struct file_handler *h = (struct file_handler *) arg;
+	struct file_state *state = tcmu_get_dev_private(h->dev);
+
+	for (;;) {
+		int result;
+		struct tcmulib_cmd *cmd;
+
+		/* get next command */
+		pthread_mutex_lock(&h->mtx);
+		while (h->cmd_tail == h->cmd_head) {
+			pthread_cond_wait(&h->cond, &h->mtx);
+		}
+		cmd = h->commands[h->cmd_tail];
+		pthread_mutex_unlock(&h->mtx);
+
+		/* process command */
+		result = file_handle_cmd(h->dev, cmd);
+		pthread_mutex_lock(&state->completion_mtx);
+		tcmulib_command_complete(h->dev, cmd, result);
+		tcmulib_processing_complete(h->dev);
+		pthread_mutex_unlock(&state->completion_mtx);
+
+		/* notify that we can process more commands */
+		pthread_mutex_lock(&h->mtx);
+		h->commands[h->cmd_tail] = NULL;
+		h->cmd_tail = (h->cmd_tail + 1) % NCOMMANDS;
+		pthread_cond_signal(&h->cond);
+		pthread_mutex_unlock(&h->mtx);
+	}
+
+	return NULL;
+}
+
+static void
+file_handler_init(struct file_handler *h, struct tcmu_device *dev, int num)
+{
+	int i;
+
+	h->dev = dev;
+	h->num = num;
+	pthread_mutex_init(&h->mtx, NULL);
+	pthread_cond_init(&h->cond, NULL);
+
+	pthread_create(&h->thr, NULL, file_handler_run, h);
+	h->cmd_head = h->cmd_tail = 0;
+	for (i = 0; i < NCOMMANDS; i++)
+		h->commands[i] = NULL;
+}
+
+static void
+file_handler_destroy(struct file_handler *h)
+{
+	if (h->thr) {
+		pthread_kill(h->thr, SIGINT);
+		pthread_join(h->thr, NULL);
+	}
+	pthread_cond_destroy(&h->cond);
+	pthread_mutex_destroy(&h->mtx);
+}
+#endif /* ASYNC_FILE_HANDLER */
 
 static bool file_check_config(const char *cfgstring, char **reason)
 {
@@ -80,6 +177,9 @@ static int file_open(struct tcmu_device *dev)
 	struct file_state *state;
 	int64_t size;
 	char *config;
+#ifdef ASYNC_FILE_HANDLER
+	int i;
+#endif /* ASYNC_FILE_HANDLER */
 
 	state = calloc(1, sizeof(*state));
 	if (!state)
@@ -114,6 +214,12 @@ static int file_open(struct tcmu_device *dev)
 		goto err;
 	}
 
+#ifdef ASYNC_FILE_HANDLER
+	pthread_mutex_init(&state->completion_mtx, NULL);
+	for (i = 0; i < NHANDLERS; i++)
+		file_handler_init(&state->h[i], dev, i);
+#endif /* ASYNC_FILE_HANDLER */
+
 	return 0;
 
 err:
@@ -124,6 +230,13 @@ err:
 static void file_close(struct tcmu_device *dev)
 {
 	struct file_state *state = tcmu_get_dev_private(dev);
+#ifdef ASYNC_FILE_HANDLER
+	int i;
+
+	for (i = 0; i < NHANDLERS; i++)
+		file_handler_destroy(&state->h[i]);
+	pthread_mutex_destroy(&state->completion_mtx);
+#endif /* ASYNC_FILE_HANDLER */
 
 	close(state->fd);
 	free(state);
@@ -133,6 +246,30 @@ static int set_medium_error(uint8_t *sense)
 {
 	return tcmu_set_sense_data(sense, MEDIUM_ERROR, ASC_READ_ERROR, NULL);
 }
+
+#ifdef ASYNC_FILE_HANDLER
+static int file_handle_cmd_async(
+	struct tcmu_device *dev,
+	struct tcmulib_cmd *tcmulib_cmd)
+{
+	struct file_state *state = tcmu_get_dev_private(dev);
+	struct file_handler *h = &state->h[state->curr_handler];
+
+	state->curr_handler = (state->curr_handler + 1) % NHANDLERS;
+
+	/* enqueue command */
+	pthread_mutex_lock(&h->mtx);
+	while ((h->cmd_head + 1) % NCOMMANDS == h->cmd_tail) {
+		pthread_cond_wait(&h->cond, &h->mtx);
+	}
+	h->commands[h->cmd_head] = tcmulib_cmd;
+	h->cmd_head = (h->cmd_head + 1) % NCOMMANDS;
+	pthread_cond_signal(&h->cond);
+	pthread_mutex_unlock(&h->mtx);
+
+	return TCMU_ASYNC_HANDLED;
+}
+#endif /* ASYNC_FILE_HANDLER */
 
 /*
  * Return scsi status or TCMU_NOT_HANDLED
@@ -184,19 +321,13 @@ static int file_handle_cmd(
 		uint64_t offset = state->block_size * tcmu_get_lba(cdb);
 		int length = tcmu_get_xfer_length(cdb) * state->block_size;
 
-		ret = lseek(state->fd, offset, SEEK_SET);
-		if (ret == -1) {
-			errp("lseek failed: %m\n");
-			return set_medium_error(sense);
-		}
-
 		/* Using this buf DTRT even if seek is beyond EOF */
 		buf = malloc(length);
 		if (!buf)
 			return set_medium_error(sense);
 		memset(buf, 0, length);
 
-		ret = read(state->fd, buf, length);
+		ret = pread(state->fd, buf, length, offset);
 		if (ret == -1) {
 			errp("read failed: %m\n");
 			free(buf);
@@ -218,12 +349,6 @@ static int file_handle_cmd(
 		uint64_t offset = state->block_size * tcmu_get_lba(cdb);
 		int length = be16toh(*((uint16_t *)&cdb[7])) * state->block_size;
 
-		ret = lseek(state->fd, offset, SEEK_SET);
-		if (ret == -1) {
-			errp("lseek failed: %m\n");
-			return set_medium_error(sense);
-		}
-
 		remaining = length;
 
 		while (remaining) {
@@ -231,13 +356,14 @@ static int file_handle_cmd(
 
 			to_copy = (remaining > iovec->iov_len) ? iovec->iov_len : remaining;
 
-			ret = write(state->fd, iovec->iov_base, to_copy);
+			ret = pwrite(state->fd, iovec->iov_base, to_copy, offset);
 			if (ret == -1) {
 				errp("Could not write: %m\n");
 				return set_medium_error(sense);
 			}
 
 			remaining -= to_copy;
+			offset += to_copy;
 			iovec++;
 		}
 
@@ -254,15 +380,21 @@ static const char file_cfg_desc[] =
 	"The path to the file to use as a backstore.";
 
 static struct tcmur_handler file_handler = {
-	.name = "File-backed Handler (example code)",
-	.subtype = "file",
 	.cfg_desc = file_cfg_desc,
 
 	.check_config = file_check_config,
 
 	.open = file_open,
 	.close = file_close,
+#ifdef ASYNC_FILE_HANDLER
+	.name = "File-backed Handler (example async code)",
+	.subtype = "file_async",
+	.handle_cmd = file_handle_cmd_async,
+#else
+	.name = "File-backed Handler (example code)",
+	.subtype = "file",
 	.handle_cmd = file_handle_cmd,
+#endif
 };
 
 /* Entry point must be named "handler_init". */
