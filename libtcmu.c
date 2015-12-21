1f9a2a2d4270786761c8b7ef95cb1aa2737f491c
Max Khon <fjoe@samodelkin.net>
2015-12-21 20:23:22 +0000

    - Rename get_cdb_length() to tcmu_get_cdb_length() and add support for variable-length CDB (the code was copied from kernel).

--- libtcmu.c
+++ libtcmu.c
@@ -596,44 +596,10 @@ void tcmulib_command_complete(
 	mb->cmd_tail = (mb->cmd_tail + tcmu_hdr_get_len(ent->hdr.len_op)) % mb->cmdr_size;
 }
 
-#define VARIABLE_LENGTH_CMD   0x7f
-
-/* defined in T10 SCSI Primary Commands-2 (SPC2) */
-struct scsi_varlen_cdb_hdr {
-	uint8_t opcode;         /* opcode always == VARIABLE_LENGTH_CMD */
-	uint8_t control;
-	uint8_t misc[5];
-	uint8_t additional_cdb_length;  /* total cdb length - 8 */
-	uint16_t service_action;
-	/* service specific data follows */
-};
-
-static inline unsigned
-scsi_varlen_cdb_length(const void *hdr)
-{
-	 return ((struct scsi_varlen_cdb_hdr *)hdr)->additional_cdb_length + 8;
-}
-
-/* Command group 3 is reserved and should never be used.  */
-static const unsigned char scsi_command_size_tbl[8] =
-{
-	 6, 10, 10, 12,
-	 16, 12, 10, 10
-};
-
-#define COMMAND_SIZE(opcode) scsi_command_size_tbl[((opcode) >> 5) & 7]
-
-static inline unsigned
-scsi_command_size(const unsigned char *cdb)
-{
-	 return (cdb[0] == VARIABLE_LENGTH_CMD) ?
-		 scsi_varlen_cdb_length(cdb) : COMMAND_SIZE(cdb[0]);
-}
-
 struct tcmulib_cmd *tcmulib_async_command_init(struct tcmulib_cmd *cmd)
 {
 	struct tcmulib_cmd *ret;
-	unsigned cdb_len = scsi_command_size(cmd->cdb);
+	unsigned cdb_len = tcmu_get_cdb_length(cmd->cdb);
 
 	/* alloc memory for cmd itself, iovec and cdb */
 	ret = malloc(sizeof(*ret) + sizeof(*ret->iovec) * cmd->iov_cnt + cdb_len);
