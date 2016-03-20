e6e75128c45de00a56b50058b0803476a8faf625
Fam Zheng <famz@redhat.com>
2016-03-20 09:42:44 +0800

    tcmu-runner: Add find_handler_by_subtype
    Signed-off-by: Fam Zheng <famz@redhat.com>

--- main.c
+++ main.c
@@ -81,6 +81,17 @@ void errp(const char *fmt, ...)
 	va_end(va);
 }
 
+static struct tcmur_handler *find_handler_by_subtype(gchar *subtype)
+{
+	struct tcmur_handler **handler;
+
+	darray_foreach(handler, g_runner_handlers) {
+		if (strcmp((*handler)->subtype, subtype) == 0)
+			return *handler;
+	}
+	return NULL;
+}
+
 void tcmur_register_handler(struct tcmur_handler *handler)
 {
 	darray_append(g_runner_handlers, handler);
