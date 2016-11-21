c1e49391ceb80b3ebaba60ecf4d5d5514fd2edcd
David Disseldorp <ddiss@suse.de>
2016-11-22 00:53:32 +0100

    libtcmu: detect INQURY response truncation
    tcmu_memcpy_into_iovec() is used to copy the pregenerated INQUIRY
    response data into caller provided iovecs. Ensure that these memcopies
    aren't truncated.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- api.c
+++ api.c
@@ -387,6 +387,7 @@ int tcmu_emulate_std_inquiry(
 	uint8_t *sense)
 {
 	uint8_t buf[36];
+	size_t copied;
 
 	memset(buf, 0, sizeof(buf));
 
@@ -400,7 +401,11 @@ int tcmu_emulate_std_inquiry(
 	memcpy(&buf[32], "0002", 4);
 	buf[4] = 31; /* Set additional length to 31 */
 
-	tcmu_memcpy_into_iovec(iovec, iov_cnt, buf, sizeof(buf));
+	copied = tcmu_memcpy_into_iovec(iovec, iov_cnt, buf, sizeof(buf));
+	if (copied != sizeof(buf)) {
+		return tcmu_set_sense_data(sense, HARDWARE_ERROR,
+					   ASC_INTERNAL_TARGET_FAILURE, NULL);
+	}
 
 	return SAM_STAT_GOOD;
 }
@@ -430,6 +435,8 @@ int tcmu_emulate_evpd_inquiry(
 	size_t iov_cnt,
 	uint8_t *sense)
 {
+	size_t copied;
+
 	switch (cdb[2]) {
 	case 0x0: /* Supported VPD pages */
 	{
@@ -444,7 +451,13 @@ int tcmu_emulate_evpd_inquiry(
 
 		data[3] = 3;
 
-		tcmu_memcpy_into_iovec(iovec, iov_cnt, data, sizeof(data));
+		copied = tcmu_memcpy_into_iovec(iovec, iov_cnt, data,
+						sizeof(data));
+		if (copied != sizeof(data)) {
+			return tcmu_set_sense_data(sense, HARDWARE_ERROR,
+						   ASC_INTERNAL_TARGET_FAILURE,
+						   NULL);
+		}
 
 		return SAM_STAT_GOOD;
 	}
@@ -534,7 +547,13 @@ int tcmu_emulate_evpd_inquiry(
 
 		*tot_len = htobe16(used);
 
-		tcmu_memcpy_into_iovec(iovec, iov_cnt, data, used + 4);
+		copied = tcmu_memcpy_into_iovec(iovec, iov_cnt, data,
+						used + 4);
+		if (copied != used + 4) {
+			return tcmu_set_sense_data(sense, HARDWARE_ERROR,
+						   ASC_INTERNAL_TARGET_FAILURE,
+						   NULL);
+		}
 
 		free(wwn);
 		wwn = NULL;
@@ -579,7 +598,13 @@ int tcmu_emulate_evpd_inquiry(
 		/* Optimal xfer length */
 		memcpy(&data[12], &val32, 4);
 
-		tcmu_memcpy_into_iovec(iovec, iov_cnt, data, sizeof(data));
+		copied = tcmu_memcpy_into_iovec(iovec, iov_cnt, data,
+						sizeof(data));
+		if (copied != sizeof(data)) {
+			return tcmu_set_sense_data(sense, HARDWARE_ERROR,
+						   ASC_INTERNAL_TARGET_FAILURE,
+						   NULL);
+		}
 
 		return SAM_STAT_GOOD;
 	}
