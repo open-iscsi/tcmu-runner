59542de6dd56a365777f772df74b15c0bad3657a
Mike Christie <mchristi@redhat.com>
2017-01-24 02:26:38 -0600

    Don't fail short/long inquirys
    You cannot fail a inquiry because there is too much/litte space.
    You would set the under/over flow bit at the command/transport
    level.
    
    You should set the number of bytes that would be transferred if
    there was the correct amount of space in inquiry response's
    ADDITIONAL LENGTH field. The initiator will then read that
    and resend the inquiry with a larger buffer.
    
    Signed-off-by: Mike Christie <mchristi@redhat.com>

--- api.c
+++ api.c
@@ -288,7 +288,6 @@ int tcmu_emulate_std_inquiry(
 	uint8_t *sense)
 {
 	uint8_t buf[36];
-	size_t copied;
 
 	memset(buf, 0, sizeof(buf));
 
@@ -302,12 +301,7 @@ int tcmu_emulate_std_inquiry(
 	memcpy(&buf[32], "0002", 4);
 	buf[4] = 31; /* Set additional length to 31 */
 
-	copied = tcmu_memcpy_into_iovec(iovec, iov_cnt, buf, sizeof(buf));
-	if (copied != sizeof(buf)) {
-		return tcmu_set_sense_data(sense, HARDWARE_ERROR,
-					   ASC_INTERNAL_TARGET_FAILURE, NULL);
-	}
-
+	tcmu_memcpy_into_iovec(iovec, iov_cnt, buf, sizeof(buf));
 	return SAM_STAT_GOOD;
 }
 
@@ -336,8 +330,6 @@ int tcmu_emulate_evpd_inquiry(
 	size_t iov_cnt,
 	uint8_t *sense)
 {
-	size_t copied;
-
 	switch (cdb[2]) {
 	case 0x0: /* Supported VPD pages */
 	{
@@ -352,14 +344,7 @@ int tcmu_emulate_evpd_inquiry(
 
 		data[3] = 3;
 
-		copied = tcmu_memcpy_into_iovec(iovec, iov_cnt, data,
-						sizeof(data));
-		if (copied != sizeof(data)) {
-			return tcmu_set_sense_data(sense, HARDWARE_ERROR,
-						   ASC_INTERNAL_TARGET_FAILURE,
-						   NULL);
-		}
-
+		tcmu_memcpy_into_iovec(iovec, iov_cnt, data, sizeof(data));
 		return SAM_STAT_GOOD;
 	}
 	break;
@@ -448,13 +433,7 @@ int tcmu_emulate_evpd_inquiry(
 
 		*tot_len = htobe16(used);
 
-		copied = tcmu_memcpy_into_iovec(iovec, iov_cnt, data,
-						used + 4);
-		if (copied != used + 4) {
-			return tcmu_set_sense_data(sense, HARDWARE_ERROR,
-						   ASC_INTERNAL_TARGET_FAILURE,
-						   NULL);
-		}
+		tcmu_memcpy_into_iovec(iovec, iov_cnt, data, used + 4);
 
 		free(wwn);
 		wwn = NULL;
@@ -499,13 +478,7 @@ int tcmu_emulate_evpd_inquiry(
 		/* Optimal xfer length */
 		memcpy(&data[12], &val32, 4);
 
-		copied = tcmu_memcpy_into_iovec(iovec, iov_cnt, data,
-						sizeof(data));
-		if (copied != sizeof(data)) {
-			return tcmu_set_sense_data(sense, HARDWARE_ERROR,
-						   ASC_INTERNAL_TARGET_FAILURE,
-						   NULL);
-		}
+		tcmu_memcpy_into_iovec(iovec, iov_cnt, data, sizeof(data));
 
 		return SAM_STAT_GOOD;
 	}
