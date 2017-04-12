18746e410924b000ee6e0ef0a00df52e9b63f54f
Mike Christie <mchristi@redhat.com>
2017-04-12 16:44:04 -0500

    Remove tcmu_emulate_write_verify
    qcow is now converted to a runner cmd loop managed handler, so
    WRITE_VERIFY is handled by runner. And, Venky's removed generic_handle_cmd's
    tcmu_emulate_write_verify use, so we can remove it.
    
    Signed-off-by: Mike Christie <mchristi@redhat.com>

--- libtcmu_common.h
+++ libtcmu_common.h
@@ -94,10 +94,6 @@ int tcmu_emulate_read_capacity_16(uint64_t num_lbas, uint32_t block_size, uint8_
 				  struct iovec *iovec, size_t iov_cnt, uint8_t *sense);
 int tcmu_emulate_mode_sense(uint8_t *cdb, struct iovec *iovec, size_t iov_cnt, uint8_t *sense);
 int tcmu_emulate_mode_select(uint8_t *cdb, struct iovec *iovec, size_t iov_cnt, uint8_t *sense);
-int tcmu_emulate_write_verify(struct tcmu_device *, struct tcmulib_cmd *,
-			      rw_fn_t read, rw_fn_t write,
-			      struct iovec *, size_t, off_t);
-
 void tcmu_callout_finished(struct tcmu_device *dev,
 			   struct tcmulib_cmd *cmd, int ret);
 
