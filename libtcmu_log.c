open-iscsi/tcmu-runner

commit dc31d81d93a804a57ceff43b4bcccbffb2a51d56

config: deprecate tcmu_parse_config
Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

libtcmu_log.c
@@ -652,13 +652,6 @@ int tcmu_setup_log(char *log_dir)
 	struct log_buf *logbuf;
 	int ret;
 
-	if (!log_dir)
-		log_dir = getenv("TCMU_LOGDIR");
-	if (!log_dir)
-		log_dir = tcmu_log_dir;
-	if (!log_dir)
-		log_dir = TCMU_LOG_DIR_DEFAULT;
-
 	ret = tcmu_log_dir_create(log_dir);
 	if (ret) {
 		tcmu_err("Could not setup log dir %s. Error %d.\n", log_dir,

libtcmu_config.c
@@ -350,7 +350,7 @@ static void tcmu_parse_options(struct tcmu_config *cfg, char *buf, int len)
 	tcmu_conf_set_options(cfg);
 }
 
-static int tcmu_load_config(struct tcmu_config *cfg)
+int tcmu_load_config(struct tcmu_config *cfg)
 {
 	int ret = -1;
 	int fd, len;
@@ -452,32 +452,6 @@ static void *dyn_config_start(void *arg)
 	return NULL;
 }
 
-struct tcmu_config *tcmu_parse_config(const char *path)
-{
-	struct tcmu_config *cfg;
-
-	cfg = calloc(1, sizeof(*cfg));
-	if (cfg == NULL) {
-		tcmu_err("Alloc TCMU config failed!\n");
-		return NULL;
-	}
-
-	if (!path)
-		path = "/etc/tcmu/tcmu.conf"; /* the default config file */
-
-	snprintf(cfg->path, PATH_MAX, path);
-	if (tcmu_load_config(cfg)) {
-		tcmu_err("Loading TCMU config failed!\n");
-		goto free_cfg;
-	}
-
-	return cfg;
-
-free_cfg:
-	free(cfg);
-	return NULL;
-}
-
 int tcmu_watch_config(struct tcmu_config *cfg)
 {
 	return pthread_create(&cfg->thread_id, NULL, dyn_config_start, cfg);
@@ -486,6 +460,26 @@ int tcmu_watch_config(struct tcmu_config *cfg)
 void tcmu_unwatch_config(struct tcmu_config *cfg)
 {
 	tcmu_thread_cancel(cfg->thread_id);
+}
+
+struct tcmu_config* tcmu_initialize_config(void)
+{
+	struct tcmu_config *cfg;
+	char *log_dir;
+
+	cfg = calloc(1, sizeof(*cfg));
+	if (cfg == NULL) {
+		tcmu_err("allocating TCMU config failed: %m\n");
+		errno = ENOMEM;
+		return NULL;
+	}
+
+	log_dir = getenv("TCMU_LOGDIR");
+	snprintf(cfg->log_dir, PATH_MAX, log_dir ? log_dir : TCMU_LOG_DIR_DEFAULT);
+	cfg->log_level = TCMU_CONF_LOG_INFO;
+	snprintf(cfg->path, PATH_MAX, TCMU_CONFIGFILE_DEFAULT);
+
+	return cfg;
 }
 
 void tcmu_free_config(struct tcmu_config *cfg)

libtcmu_config.h
@@ -51,8 +51,9 @@ static const char *const log_level_lookup[] = {
 	[TCMU_CONF_LOG_DEBUG_SCSI_CMD]	= "DEBUG SCSI CMD",
 };
 
+struct tcmu_config* tcmu_initialize_config(void);
 void tcmu_free_config(struct tcmu_config *cfg);
-struct tcmu_config * tcmu_parse_config(const char *path);
+int tcmu_load_config(struct tcmu_config *cfg);
 int tcmu_watch_config(struct tcmu_config *cfg);
 void tcmu_unwatch_config(struct tcmu_config *cfg);
 

libtcmu_log.h
@@ -22,7 +22,8 @@
 #define TCMU_LOG_DEBUG_SCSI_CMD	(LOG_DEBUG + 1)	/* scsi cmd debug-level messages */
 
 /* default tcmu log dir path */
-#define TCMU_LOG_DIR_DEFAULT   "/var/log"
+#define TCMU_LOG_DIR_DEFAULT	"/var/log"
+#define TCMU_CONFIGFILE_DEFAULT	"/etc/tcmu/tcmu.conf"
 
 struct tcmu_device;
 

main.c
@@ -992,9 +992,13 @@ int main(int argc, char **argv)
 	bool new_path = false;
 	bool watching_cfg = false;
 	struct flock lock_fd = {0, };
-	char *log_dir = NULL;
 	int fd;
 	int ret;
+
+	if ((tcmu_cfg = tcmu_initialize_config()) == NULL) {
+		tcmu_err("initializing the tcmu config failed: %m\n");
+		exit(EXIT_FAILURE);
+	}
 
 	while (1) {
 		int option_index = 0;
@@ -1013,9 +1017,7 @@ int main(int argc, char **argv)
 			}
 			break;
 		case 'l':
-			log_dir = strdup(optarg);
-			if (!log_dir)
-				goto free_opt;
+			snprintf(tcmu_cfg->log_dir, PATH_MAX, optarg);
 			break;
 		case 'f':
 			nr_files = atol(optarg);
@@ -1023,23 +1025,23 @@ int main(int argc, char **argv)
 				tcmu_err("--nofile=%d should be in [%lu, %lu]\n", nr_files,
 					(unsigned long)TCMUR_MIN_OPEN_FD,
 					(unsigned long)TCMUR_MAX_OPEN_FD);
-				goto free_opt;
+				goto free_config;
 			}
 
 			ret = tcmu_set_max_fd_limit(nr_files);
 			if (ret)
-				goto free_opt;
+				goto free_config;
 			break;
 		case 'd':
-			tcmu_set_log_level(TCMU_CONF_LOG_DEBUG_SCSI_CMD);
+			tcmu_cfg->log_level = TCMU_CONF_LOG_DEBUG_SCSI_CMD;
 			break;
 		case 'V':
 			tcmu_info("tcmu-runner %s\n", TCMUR_VERSION);
-			goto free_opt;
+			goto free_config;
 		default:
 		case 'h':
 			usage();
-			goto free_opt;
+			goto free_config;
 		}
 	}
 
@@ -1048,11 +1050,12 @@ int main(int argc, char **argv)
 	 * the log directory may be configured via the system config file
 	 * which will be used in logger setting up.
 	 */
-	tcmu_cfg = tcmu_parse_config(NULL);
-	if (!tcmu_cfg)
-		goto free_opt;
+	if (tcmu_load_config(tcmu_cfg)) {
+		tcmu_err("Loading TCMU config failed!\n");
+		goto free_config;
+	}
 
-	if (tcmu_setup_log(log_dir))
+	if (tcmu_setup_log(tcmu_cfg->log_dir))
 		goto free_config;
 
 	tcmu_info("Starting...\n");
@@ -1219,7 +1222,6 @@ unwatch_cfg:
 	tcmu_destroy_log();
 free_config:
 	tcmu_free_config(tcmu_cfg);
-free_opt:
 	if (new_path)
 		free(handler_path);
 

commit eb62a996ece9de700de2906e410263d8f1087c05

Validate input for tcmu_log_dir_check()
Do not crash on invalid input.

Fixes: dc31d81 ("config: deprecate tcmu_parse_config")

Signed-off-by: Sebastian Herbszt <herbszt@gmx.de>

libtcmu_log.c
@@ -535,6 +535,9 @@ static void *log_thread_start(void *arg)
 
 static bool tcmu_log_dir_check(const char *path)
 {
+	if (!path)
+		return false;
+
 	if (strlen(path) >= PATH_MAX - TCMU_LOG_FILENAME_MAX) {
 		tcmu_err("--tcmu-log-dir='%s' cannot exceed %d characters\n",
 			 path, PATH_MAX - TCMU_LOG_FILENAME_MAX - 1);