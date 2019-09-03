eff210ccbf843f08c2dd8c3a149252cc0e01abea
Josh Jameson <josh@servebyte.com>
2019-09-03 22:38:08 +0100

    Add support for Debian

--- extra/install_dep.sh
+++ extra/install_dep.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Userspace side of the LIO TCM-User backstore
 #
@@ -23,8 +23,22 @@ if [ y`uname`y = yLinuxy ]; then
 		yum search librbd-devel | grep -q "N/S matched" && LIBRBD=librbd || LIBRBD=librbd1
 	        $SUDO yum install -y $LIBRBD-devel
 		;;
+	debian)
+		# Update APT cache
+		$SUDO apt update
+		
+		# for generic
+		$SUDO apt install -y cmake make gcc libnl-3-dev libglib2.0-dev zlib1g kmod
+		$SUDO apt install -y libnl-3-dev libglib2.0-0 libkmod-dev libgoogle-perftools-dev
+		
+		# for glusterfs
+		$SUDO apt install -y libglusterfs-dev
+		
+		# for ceph
+		$SUDO apt install -y librados2 librbd-dev
+		;;
 	*)
-		echo "TODO: only fedora/rhel/centos are supported for now!"
+		echo "TODO: only fedora/rhel/centos/debian are supported for now!"
 		;;
 	esac
 else
