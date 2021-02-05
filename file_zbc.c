open-iscsi/tcmu-runner

commit 87da9fb573a7a7bcad8ec102443cf388d2f68c92

zbc: fix write emulation
ZBC allows write commands to conventional zones to cross zone
boundaries. Fix zbc_write() to allow this.

To keep the code simple, use 2 loops: the first one checks zone
boundaries and write pointer alignment and the second loop performs
the writes for all segments of the command buffer iovec.

Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>
Signed-off-by: Dmitry Fomichev <dmitry.fomichev@wdc.com>

file_zbc.c
@@ -2028,6 +2028,76 @@ static int zbc_read(struct tcmu_device *dev, struct tcmulib_cmd *cmd)
 }
 
 /*
+ * Check conv to seq and seq to seq zone boundary crossings.
+ * and write pointer alignment in sequential write required zones.
+ */
+static int zbc_write_check_zones(struct tcmu_device *dev,
+				 struct tcmulib_cmd *cmd,
+				 size_t nr_lbas, uint64_t lba)
+{
+	struct zbc_dev *zdev = tcmu_get_dev_private(dev);
+	struct zbc_zone *zone;
+	int zone_type = 0;
+	size_t count;
+
+	while (nr_lbas) {
+
+		/* Get the zone of the current LBA */
+		zone = zbc_get_zone(zdev, lba, false);
+		if (!zone) {
+			tcmu_dev_err(zdev->dev,
+				     "Get zone at LBA %llu failed\n",
+				     lba);
+			return tcmu_set_sense_data(cmd->sense_buf,
+						   HARDWARE_ERROR,
+						   ASC_INTERNAL_TARGET_FAILURE,
+						   NULL);
+		}
+
+		/* Check conv -> seq and seq -> seq zone boundary crossing */
+		if (zone_type == 0)
+			zone_type = zone->type;
+		if (zone->type != zone_type ||
+		    (zbc_zone_seq_req(zone) &&
+		     lba + nr_lbas > zone->start + zone->len)) {
+			tcmu_dev_err(dev,
+				     "Write boundary violation lba %llu, "
+				     "xfer len %lu\n",
+				     lba, nr_lbas);
+			return tcmu_set_sense_data(cmd->sense_buf,
+						   ILLEGAL_REQUEST,
+						   ASC_WRITE_BOUNDARY_VIOLATION,
+						   NULL);
+		}
+
+		/*
+		 * For sequential write required zones, enforce write pointer
+		 * position. Same for conventional zones with conv_check_wp
+		 * enabled.
+		 */
+		if (zbc_zone_seq_req(zone) && lba != zone->wp) {
+			tcmu_dev_err(dev, "Unaligned write lba %llu, wp %llu\n",
+				     lba, zone->wp);
+			return tcmu_set_sense_data(cmd->sense_buf,
+						   ILLEGAL_REQUEST,
+						   ASC_UNALIGNED_WRITE_COMMAND,
+						   NULL);
+		}
+
+		if (lba + nr_lbas > zone->start + zone->len)
+			count = zone->start + zone->len - lba;
+		else
+			count = nr_lbas;
+
+		lba += count;
+		nr_lbas -= count;
+
+	}
+
+	return SAM_STAT_GOOD;
+}
+
+/*
  * Write command emulation.
  */
 static int zbc_write(struct tcmu_device *dev, struct tcmulib_cmd *cmd)
@@ -2036,11 +2106,10 @@ static int zbc_write(struct tcmu_device *dev, struct tcmulib_cmd *cmd)
 	uint8_t *cdb = cmd->cdb;
 	uint64_t lba = tcmu_get_lba(cdb);
 	size_t nr_lbas = tcmu_get_xfer_length(cdb);
+	size_t count, lba_count;
 	size_t iov_cnt = cmd->iov_cnt;
 	struct iovec *iovec = cmd->iovec;
-	size_t remaining = tcmu_iovec_length(iovec, iov_cnt);
 	struct zbc_zone *zone;
-	off_t offset;
 	ssize_t ret;
 
 	tcmu_dev_dbg(dev, "Write LBA %llu+%u, %zu vectors\n",
@@ -2052,45 +2121,49 @@ static int zbc_write(struct tcmu_device *dev, struct tcmulib_cmd *cmd)
 	if (ret != SAM_STAT_GOOD)
 		return ret;
 
-	/* For writes, check zone boundary crossing */
-	zone = zbc_get_zone(zdev, lba, false);
-	if (lba + nr_lbas > zone->start + zone->len) {
-		tcmu_dev_err(dev, "Write boundary violation lba %llu, xfer len %lu\n",
-			     lba, nr_lbas);
-		return tcmu_set_sense_data(cmd->sense_buf,
-					   ILLEGAL_REQUEST,
-					   ASC_WRITE_BOUNDARY_VIOLATION,
-					   NULL);
-	}
+	/* Check zone boundary crossing */
+	ret = zbc_write_check_zones(dev, cmd, nr_lbas, lba);
+	if (ret != SAM_STAT_GOOD)
+		return ret;
 
-	/* For sequential write required zones, check write pointer position */
-	if (zbc_zone_seq_req(zone) && lba != zone->wp) {
-		tcmu_dev_err(dev, "Unaligned write lba %llu, wp %llu\n",
-			     lba, zone->wp);
-		return tcmu_set_sense_data(cmd->sense_buf,
-					   ILLEGAL_REQUEST,
-					   ASC_UNALIGNED_WRITE_COMMAND,
-					   NULL);
-	}
+	/* Do write */
+	while (nr_lbas) {
 
-	/* If the zone is not open, implicitly open it */
-	if (zbc_zone_seq(zone) && !zbc_zone_is_open(zone)) {
-
-		/* Too many explicit open ? */
-		if (zdev->nr_exp_open >= zdev->nr_open_zones)
+		/* Get the zone of the current LBA */
+		zone = zbc_get_zone(zdev, lba, false);
+		if (lba + nr_lbas > zone->start + zone->len) {
+			tcmu_dev_err(dev,
+				     "Write boundary violation lba %llu, "
+				     "xfer len %lu\n",
+				     lba, nr_lbas);
 			return tcmu_set_sense_data(cmd->sense_buf,
-						   DATA_PROTECT,
-						   ASC_INSUFFICIENT_ZONE_RESOURCES,
+						   ILLEGAL_REQUEST,
+						   ASC_WRITE_BOUNDARY_VIOLATION,
 						   NULL);
+		}
 
-		__zbc_open_zone(zdev, zone, false);
+		/* If the zone is not open, implicitly open it */
+		if (zbc_zone_seq(zone) && !zbc_zone_is_open(zone)) {
+			/* Too many explicit open ? */
+			if (zdev->nr_exp_open >= zdev->nr_open_zones)
+				return tcmu_set_sense_data(cmd->sense_buf,
+							   DATA_PROTECT,
+							   ASC_INSUFFICIENT_ZONE_RESOURCES,
+							   NULL);
+			__zbc_open_zone(zdev, zone, false);
+		}
 
-	}
+		/* Do write */
+		if (lba + nr_lbas > zone->start + zone->len)
+			count = zone->start + zone->len - lba;
+		else
+			count = nr_lbas;
+		lba_count = iovec->iov_len / zdev->lba_size;
+		if (lba_count < count)
+			count = lba_count;
 
-	offset = zdev->meta_size + lba * zdev->lba_size;
-	while (remaining) {
-
-		ret = pwritev(zdev->fd, iovec, iov_cnt, offset);
+		ret = pwrite(zdev->fd, iovec->iov_base, count * zdev->lba_size,
+			     zdev->meta_size + lba * zdev->lba_size);
 		if (ret <= 0) {
 			tcmu_dev_err(dev, "Write failed: %m\n");
 			return tcmu_set_sense_data(cmd->sense_buf,
@@ -2100,24 +2173,27 @@ static int zbc_write(struct tcmu_device *dev, struct tcmulib_cmd *cmd)
 		}
 
 		tcmu_seek_in_iovec(iovec, ret);
-		offset += ret;
-		remaining -= ret;
+		count = ret / zdev->lba_size;
 
-	}
-
-	if (zbc_zone_seq(zone)) {
 		/* Adjust write pointer */
 		if (zbc_zone_seq_req(zone)) {
-			zone->wp += nr_lbas;
+			zone->wp += count;
 		} else if (zbc_zone_seq_pref(zone)) {
-			if (lba + nr_lbas >= zone->wp)
-				zone->wp = lba + nr_lbas;
+			if (lba + count >= zone->wp)
+				zone->wp = lba + count;
 		}
-		if (zone->wp >= zone->start + zone->len) {
+
+		if (zbc_zone_seq(zone) &&
+		    zone->wp >= zone->start + zone->len) {
 			if (zbc_zone_is_open(zone))
 				__zbc_close_zone(zdev, zone);
+			zone->wp = zone->start + zone->len;
 			zone->cond = ZBC_ZONE_COND_FULL;
 		}
+
+		lba += count;
+		nr_lbas -= count;
+
 	}
 
 	return SAM_STAT_GOOD;

commit 8ba558522094105790f6ee5ec0224dc8eed69a2c

file_zbc: Remove unnecessary zone boundary check
Write commands to ZBC devices across zone boundaries shall fail, if the
crossed boundary is between sequential write required zones, or between
a conventional zone and a sequential write required zone. Write commands
across conventional zones shall not fail. Though the commit 87da9fb573a7
("zbc: fix write emulation") relaxed the cross zone boundary check to
allow writes across conventional zones, still such write commands fail.
The commit introduced the helper function zbc_write_check_zones() and
implemented the relaxed boundary check. However, still the old boundary
check is left in zbc_write() and triggers failures for write commands
across conventional zones. To avoid the unnecessary check, remove the
left check code from zbc_write().

Fixes: 87da9fb573a7 ("zbc: fix write emulation")
Signed-off-by: Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>

file_zbc.c
@@ -2129,14 +2129,6 @@ static int zbc_write(struct tcmu_device *dev, struct tcmulib_cmd *cmd)
 
 		/* Get the zone of the current LBA */
 		zone = zbc_get_zone(zdev, lba, false);
-		if (lba + nr_lbas > zone->start + zone->len) {
-			tcmu_dev_err(dev,
-				     "Write boundary violation lba %"PRIu64", xfer len %zu\n",
-				     lba, nr_lbas);
-			return tcmu_sense_set_data(cmd->sense_buf,
-						   ILLEGAL_REQUEST,
-						   ASC_WRITE_BOUNDARY_VIOLATION);
-		}
 
 		/* If the zone is not open, implicitly open it */
 		if (zbc_zone_seq(zone) && !zbc_zone_is_open(zone)) {